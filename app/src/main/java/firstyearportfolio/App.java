19697cfea7805d5989353e55d492ff36fbaf8715
Chris <128728293+DMTumbler@users.noreply.github.com>
2024-03-18 23:54:59 -0400

    Added more option to switch case

--- app/src/main/java/firstyearportfolio/App.java
+++ app/src/main/java/firstyearportfolio/App.java
@@ -11,9 +11,11 @@ public class App {
             System.out.println("A combination of options will be visible to try out those");
             System.out.println("Different classes.\n");
             System.out.println("Please select 1 through 3 to see the different option. Press 4 for More.");
-            System.out.println("1: A monolithic class that contains an assortment of programs based on math exercises.");
+            System.out
+                    .println("1: A monolithic class that contains an assortment of programs based on math exercises.");
             System.out.println("2: Generates a random array and finds the average variance and deviation.");
             System.out.println("3: Averages a multidimensional array.");
+            System.out.println("4: More.");
             if (key.hasNextLine()) {
                 value = Integer.valueOf(key.nextLine());
                 switch (value) {
@@ -28,14 +30,18 @@ public class App {
                         break;
                     case 4:
                         System.out.println("Please select 1 through 3 to see the different option. Press 4 for More.");
+                        System.out.println("1: To be added.");
+                        System.out.println("2: To be added.");
+                        System.out.println("3: To be added.");
+                        System.out.println("4: More.");
                         value = Integer.valueOf(key.nextLine());
                         switch (value) {
                             case 1:
-                                System.out.println("This is a placeholder. More apps are to be added");
+                                System.out.println("This is a placeholder. More apps are to be added.");
                                 break;
 
                             default:
-                                System.out.println("This is a placeholder. More apps are to be added");
+                                System.out.println("This is a placeholder. More apps are to be added.");
                                 break;
                         }
                 }
