ceda1f907240add26587522f3bb4ec6e3e0b83ba
Chris <christhiansanchez@gmail.com>
2024-04-16 11:37:30 -0400

    Fixed text.

--- app/src/main/java/firstyearportfolio/App.java
+++ app/src/main/java/firstyearportfolio/App.java
@@ -50,19 +50,19 @@ public class App {
                       Person.demoArrayCreation();
                       break;
                     case 2:
-                      System.out.println(" is a placeholder. More apps are to be added.");
+                      System.out.println("This is a placeholder. More programs are to be added.");
                       Person.demoPersonArray();
                       break;
                     case 3:
-                      System.out.println("This is a placeholder. More apps are to be added.");
+                      System.out.println("This is a placeholder. More programs are to be added.");
                       break;
                     case 4:
-                      System.out.println("This is a place holder. More apps are to be added.");
+                      System.out.println("This is a placeholder. More programs are to be added.");
                     default:
                       break;
                   }
                 default:
-                  System.out.println("This is a placeholder. More apps are to be added.");
+                  System.out.println("This is a placeholder. More programs are to be added.");
                   break;
               }
           }
